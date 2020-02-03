49e5510d35eceb025db2f6a777167146ac5e673e
RAMA MADA <RAMA@MacBook-Pro-3.local>
2020-02-03 01:58:38 -0400

    adding schema

--- src/main/scala/retail/SparkKinesisIntegration.scala
+++ src/main/scala/retail/SparkKinesisIntegration.scala
@@ -1,50 +1,53 @@
 package retail
 
-import org.apache.spark.storage.StorageLevel
-import org.apache.spark.streaming.kinesis.KinesisInputDStream
 import org.apache.spark.streaming.{Duration, Milliseconds, StreamingContext}
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.services.kinesis.AmazonKinesisClient
-import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
 import com.amazonaws.services.dynamodbv2.document.Item
 import com.amazonaws.services.dynamodbv2.document.DynamoDB
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.functions._
-
+import org.apache.spark.sql.types._
+import org.apache.spark.streaming.Time
 
 import scala.util.parsing.json.JSONObject
 
 object SparkKinesisIntegration extends App {
 
   val credentials = new DefaultAWSCredentialsProviderChain().getCredentials()
-  val kinesisClient = new AmazonKinesisClient(credentials)
-  kinesisClient.setEndpoint(args(1))
-  val numShards = kinesisClient.describeStream(args(3)).getStreamDescription().getShards().size
-  val numStreams = numShards
+  
   val batchInterval = Milliseconds(args(5).toInt)
- val executionMode = args(0)
+  val executionMode = args(0)
   val conf = new SparkConf().setAppName("Sparkstreaming and Kinesis integration").setMaster(executionMode)
   val sc = new SparkContext(conf)
   val spark = SparkSession.builder().appName("Sparkstreaming and Kinesis integration").getOrCreate()
   val ssc = new StreamingContext(conf,batchInterval)
 
-  val kinesisStream = (0 until numStreams).map { i =>
-    KinesisInputDStream.builder
-      .streamingContext(ssc)
-      .endpointUrl(args(1))
-      .regionName(args(2))
-      .streamName(args(3))
-      .checkpointAppName(args(4))
-      .checkpointInterval(Duration(args(5).toInt))
-      .storageLevel(StorageLevel.MEMORY_AND_DISK_2)
-      .build()
-  }
-  // Union all the streams
-  val unionStreams = ssc.union(kinesisStream)
+  val userSchema = new StructType()
+                        .add("start_timestamp",TimestampType)
+                        .add("user_id", "string")
+                        .add("event_type","string")
+                        .add("flyer_id","string")
+                        .add("merchant_id","string")
+                        .add("end_timestamp",TimestampType)
 
-  val lines = unionStreams.flatMap(byteArray => new String(byteArray).split("\n"))
-  val dataset = spark.read.json("<json dataset from lines>")
+  val kinesisDF = spark
+    .readStream
+    .format("kinesis")
+    .option("streamName", "<streamName>")
+    .option("endpointUrl", "<uri>")
+    .option("initialPositionInStream", "earliest")
+    .option("format", "csv").schema("")
+    .schema(userSchema)
+    .load
+
+
+
+
 
   /**
     *  The dataset that we have computed needs to be processed in following way
@@ -55,11 +58,11 @@ object SparkKinesisIntegration extends App {
     */
 
   // step 1
-  dataset.write.parquet("<HDFS EXTERNAL TABLE LOCATION>")  // HDFS EXTERNAL TABLE LOCATION
+  kinesisDF.write.parquet("<HDFS EXTERNAL TABLE LOCATION>")  // HDFS EXTERNAL TABLE LOCATION
 
   // step 2 - Applying algorithm on dataset and persist in Amazon DB
 
-  val flyerOpenUsers = dataset.filter(col("event_type") === "flyer_open")
+  val flyerOpenUsers = kinesisDF.filter(col("event_type") === "flyer_open")
   // Calculating time spent on each flyer in seconds, as we are interested only in event_type = 'flyer_open'
   val flyerOpenUsersWithduration = flyerOpenUsers.withColumn("time_spent",(col("end_timestamp").cast("long") - col("start_timestamp").cast("long")))
   // Grouping by user_id, flyer_id and merchant_id and calculating count and average to record metrics
